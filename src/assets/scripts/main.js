807c45b3c368e065ab0ef9101b0441e30c7b0584
Carranzani98 <icabo1598@gmail.com>
2022-11-27 23:02:22 +0100

    Nueva pantalla de contenido libre: Formulario de contacto y carrusel

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,12 +6,29 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 import 'bootstrap';
+import Glide from "@glidejs/glide"
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+  //Inicialización y configuración del carrusel
+  window.addEventListener('load', () => {
+    new Glide('.carousel', {
+      type: 'carousel',
+      autoplay: 3000,
+      perView: 3,
+      focusAt: 'center',
+      breakpoints: {
+        1340: {
+          perView: 3,
+        },
+        900: {
+          perView: 2,
+        },
+        600: {
+          perView: 1,
+        },
+      },
+    }).mount()
+  })
